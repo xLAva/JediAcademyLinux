2c90e8a5c247091f16ec0cc3d52fb45de216e897
xLAva <jochen.leopold@model-view.com>
2013-04-24 19:05:37 +0200

    Copied linux folder from outcast ...

---
+++ code/linux/linux_console.cpp
@@ -0,0 +1,37 @@
+#include "../client/client.h"
+#include "linux_local.h"
+
+
+/*
+==================
+Sys_InitConsole
+==================
+*/
+void	Sys_InitConsole( void ) {
+
+}
+
+/*
+==================
+Sys_ShowConsole
+==================
+*/
+void	Sys_ShowConsole( int level, qboolean quitOnClose ) {
+	
+}
+
+
+
+/*
+================
+Sys_ConsoleInput
+
+Checks for a complete line of text typed in at the console.
+Return NULL if a complete line is not ready.
+================
+*/
+char *Sys_ConsoleInput( void ) {
+
+	return NULL;
+}
+
---
+++ code/linux/linux_local.h
@@ -0,0 +1,45 @@
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/vt.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <signal.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#include "linux_glw.h"
+
+#include <GL/glx.h>
+
+#include <X11/keysym.h>
+#include <X11/cursorfont.h>
+
+#include <X11/extensions/xf86dga.h>
+#include <X11/extensions/xf86vmode.h>
+
+
+extern	int		sys_ticBase;
+extern	int		sys_msecBase;
+extern	int		sys_lastEventTic;
+
+extern	cvar_t	*sys_waitNextEvent;
+
+void Sys_QueEvent( int time, sysEventType_t type, int value, int value2, int ptrLength, void *ptr );
+
+
+extern	int	vkeyToQuakeKey[256];
+void Sys_SendKeyEvents (void);
+
+
+void IN_Init(void);
+void IN_DeactivateMouse( void );
+void IN_Frame (void);
+
+extern	qboolean	inputActive;
+
+extern glconfig_t glConfig;
+
+
+
+
